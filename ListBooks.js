29814df89ba7a408af50f4bebcab75e4688b8551
yankev12 <33076221+yankev12@users.noreply.github.com>
2017-11-17 19:48:01 -0200

    second commit
    segundo commit

---
+++ ListBooks.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import sortBy from 'sort-by';
+import escapeRegExp from 'escape-string-regexp'
+import PropTypes from 'prop-types'
+import BookShelf from './BookShelf'
+
+ class ListBooks extends Component {
+
+
+  static propTypes = {
+    books: PropTypes.array.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+   render() {
+
+
+    let shelfType = [
+   {title: 'currentlyReading'}, 
+   {title: 'wantToRead'},
+   {title: 'read'}
+]      
+   
+    const { query } = this.state
+    const match = new RegExp(escapeRegExp(query), 'i')
+    const { books } = this.props;
+    
+
+    console.log('Props', this.props)
+     return (
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+                
+                shelfType.map((shelfi) => (
+                   <BookShelf books={() => books.filter(book=> book.shelf === shelfi.title)} />
+                ))
+        
+               
+        </div>
+      
+     )
+   }
+ }
+ 
+ export default ListBooks
